bd6da2d19224adfdf831925529b1b9f2a67dd4e9
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-05-19 20:48:03 +0700

    ็HW05

---
+++ HW04/Q3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+
+        Dictionary<string, string> productDict = new Dictionary<string, string>();
+        Queue<string> keys = new Queue<string>();
+        int letterCount = 1, numberCount = 0;
+        string input;
+
+        for (int i = 0; i < Math.Pow(26, n) * Math.Pow(10, m); i++)
+        {
+            string code = GenerateCode(letterCount, numberCount, n, m);
+            keys.Enqueue(code);
+
+            input = Console.ReadLine();
+            if (input == "Stop")
+                break;
+
+            if (productDict.ContainsKey(code))
+                productDict[code] = input;
+            else
+                productDict.Add(code, input);
+
+            numberCount++;
+            if (numberCount >= Math.Pow(10, m))
+            {
+                numberCount = 0;
+                letterCount++;
+            }
+        }
+
+        string searchCode = Console.ReadLine();
+        Console.WriteLine(productDict.ContainsKey(searchCode) ? productDict[searchCode] : "Not found!");
+    }
+
+    static string GenerateCode(int letterCount, int numberCount, int n, int m)
+    {
+        string letters = ConvertToBase26(letterCount - 1).PadLeft(n, 'A');
+        string numbers = numberCount.ToString().PadLeft(m, '0');
+        return letters + numbers;
+    }
+
+    static string ConvertToBase26(int number)
+    {
+        string result = "";
+        while (number > 0)
+        {
+            number--;
+            result = (char)('A' + number % 26) + result;
+            number /= 26;
+        }
+        return result;
+    }
+}
---
+++ HW04/Q4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Position
+{
+    public char Column { get; set; }
+    public int Row { get; set; }
+
+    public Position(char column, int row)
+    {
+        Column = column;
+        Row = row;
+    }
+
+    public Position Move(int direction, int steps)
+    {
+        Position newPosition = new Position(Column, Row);
+
+        switch (direction)
+        {
+            case 1: newPosition.Row += steps; break;
+            case 2: newPosition.Row += steps; newPosition.Column -= (char)steps; break;
+            case 3: newPosition.Column -= (char)steps; break;
+            case 4: newPosition.Row -= steps; newPosition.Column -= (char)steps; break;
+            case 5: newPosition.Row -= steps; break;
+            case 6: newPosition.Row -= steps; newPosition.Column += (char)steps; break;
+            case 7: newPosition.Column += (char)steps; break;
+            case 8: newPosition.Row += steps; newPosition.Column += (char)steps; break;
+        }
+
+        if (newPosition.Column < 'A' || newPosition.Column > 'H' || newPosition.Row < 1 || newPosition.Row > 8)
+            return null;
+
+        return newPosition;
+    }
+
+    public override string ToString() => $"{Column}{Row}";
+}
+
+class Program
+{
+    static void Main()
+    {
+        Stack<Position> mainUndo = new Stack<Position>();
+        Stack<Position> secondaryUndo = new Stack<Position>();
+
+        Position current = new Position('A', 1);
+        mainUndo.Push(current);
+
+        while (true)
+        {
+            int command = int.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    int steps = int.Parse(Console.ReadLine());
+                    Position newPosition = current.Move(command, steps);
+
+                    if (newPosition != null)
+                    {
+                        current = newPosition;
+                        mainUndo.Push(current);
+                        secondaryUndo.Clear();
+                    }
+                    break;
+                case 9:
+                    if (mainUndo.Count > 1)
+                    {
+                        secondaryUndo.Push(mainUndo.Pop());
+                        current = mainUndo.Peek();
+                    }
+                    break;
+                case 10:
+                    if (secondaryUndo.Count > 0)
+                    {
+                        current = secondaryUndo.Pop();
+                        mainUndo.Push(current);
+                    }
+                    break;
+                case 11:
+                    Console.WriteLine(current);
+                    return;
+            }
+        }
+    }
+}
